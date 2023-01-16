142cd7e32d7392cbee0a2f32139f9839ad6d30bc
HB-Student <chri00m9@mail.hansenberg.dk>
2023-01-16 14:06:10 +0100

    Animal Class

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public abstract class Animal : MonoBehaviour{
+	public Transform animalTransformer;
+	
+	public void playAnimalSound(string sound){
+		print(sound);
+	} 
+	
+} 
